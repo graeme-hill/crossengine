bb752123f5dcaba2f9a0b32cc31d83d4b8cc6d19
Graeme Hill <graemekh@gmail.com>
2017-06-03 21:30:46 -0700

    xe::Intersect static helpers

--- Math.hpp
+++ Math.hpp
@@ -9,6 +9,16 @@ struct Intersection
 {
 	bool found;
 	glm::vec3 point;
+
+	static Intersection no()
+	{
+		return { false, glm::vec3(0.0f, 0.0f, 0.0f) };
+	}
+
+	static Intersection yes(glm::vec3 p)
+	{
+		return { true, p };
+	}
 };
 
 END_XE_NAMESPACE
